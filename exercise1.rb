36d43a483970b7e4f1dbde17cbffe2ac10fd0ab5
marmo002 <marmo_02@hotmail.com>
2018-04-04 13:56:02 -0400

    printed flip array, first item on colours array, sorted ages array, added 0 to ages array, printed value of a movie on the movies hash

---
+++ exercise1.rb
@@ -0,0 +1,42 @@
+colours = ["orange", "blue", "yellow", "grey"]
+ages = [31, 53, 28, 19, 22, 27]
+flip = [true, false, true, true, false]
+fav_artists = ["Alejandro Sanz", "Marc Anthony", "Camila Cabello", "Maluma"]
+colours_symb = [:orange, :blue, :yellow, :grey]
+
+words = {
+  "world" => "Also called earth",
+  "Apple" => "Sweet red, sometimes green, fruit",
+  "computer" => "Electronic device, revolutionized humanity"
+}
+
+movies = {
+  "Pearl Harbour" => 2001,
+  "Click" => 2006,
+  "Step up" => 2006,
+  "Benjamin Button" => 2008
+}
+
+cities = {
+  "Manhattan" => 1.645,
+  "Lima" => 12,
+  "Toronto" => 2.5,
+}
+
+siblings = {
+  "Liz" => 27,
+  "Beto" => 22,
+  "Edgar" => 29,
+  "Lan" => 26,
+}
+
+puts flip
+
+puts colours[0]
+
+puts ages.sort
+
+ages << 0
+p ages
+
+p movies["Click"]
