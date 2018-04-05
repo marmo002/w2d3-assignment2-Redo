ef526c65b01e1fe5819fb530b08e9e0042727719
marmo002 <marmo_02@hotmail.com>
2018-04-05 19:09:29 -0400

    created exercise13Bonus. combine colours and fav_artists arrays then print the result array sorted.  Print the name of artist with the ages on the same line. Added 1 to all ages. sum all ages and printed the total. Created a new array with only the heads from flip array

---
+++ exercise13Bonus.rb
@@ -0,0 +1,30 @@
+colours = ["orange", "blue", "yellow", "grey"]
+ages = [31, 53, 28, 19, 22, 27]
+flip = [true, false, true, true, false]
+fav_artists = ["Alejandro Sanz", "Marc Anthony", "Camila Cabello", "Maluma"]
+colours_symb = [:orange, :blue, :yellow, :grey]
+
+mix = colours + fav_artists
+
+puts mix.sort
+
+puts
+fav_artists.each do |artist|
+  ages.each do |age|
+    puts "I love #{artist} #{age}"
+  end
+end
+puts
+ages = ages.map do |age|
+  age += 1
+end
+
+puts ages
+puts
+
+sum_ages = ages.reduce(:+)
+
+puts "The total sum of ages is #{sum_ages}"
+puts
+heads = flip.select { |e| e==true }
+puts heads
