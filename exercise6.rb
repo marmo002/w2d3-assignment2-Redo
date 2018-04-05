2fdb19a5a309c97002efe13cbd06fd3ae1b76456
marmo002 <marmo_02@hotmail.com>
2018-04-04 21:13:03 -0400

    Createda hash with years and array of movies that came out in that year as values,  an array with phone pad numbers, an array of countries with hashes with their information

---
+++ exercise6.rb
@@ -0,0 +1,65 @@
+colours = ["orange", "blue", "yellow", "grey"]
+ages = [31, 53, 28, 19, 22, 27]
+flip = [true, false, true, true, false]
+fav_artists = ["Alejandro Sanz", "Marc Anthony", "Camila Cabello", "Maluma"]
+colours_symb = [:orange, :blue, :yellow, :grey]
+
+words = {
+  "world" => "Also called earth",
+  "Apple" => "Sweet red, sometimes green, fruit",
+  "computer" => "Electronic device, revolutionized humanity"
+}
+
+movies = {
+  "Pearl Harbour" => 2001,
+  "Click" => 2006,
+  "Step up" => 2006,
+  "Benjamin Button" => 2008
+}
+
+cities = {
+  "Manhattan" => 1.645,
+  "Lima" => 12,
+  "Toronto" => 2.5,
+}
+
+siblings = {
+  "Jose" => 52,
+  "Gato" => 40,
+  "Liz" => 27,
+  "Beto" => 22,
+  "Edgar" => 29,
+  "Lan" => 26,
+}
+
+movie_year = {
+  1999 => ["The Matrix", "Star Wars: Episode 1", "The Mummy"],
+  2009 => ["Avatar", "Star Trek", "District 9"],
+  2017 => ["Anabelle Origins", "Conjouring 2", "Storks"]
+  2019 => ["How to Train Your Dragon 3", "Toy Story 4", "Star Wars: Episode 9"]
+}
+
+phone_pad = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  ["*", 0, "#"]
+]
+
+countries = [
+  {
+    name: "Peru",
+    continent: "South America",
+    is_island?: false
+  }
+  {
+    name: "Canada",
+    continent: "North America",
+    is_island?: false
+  }
+  {
+    name: "Cuba",
+    continent: "Central America",
+    is_island?: true
+  }
+]
