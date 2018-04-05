a2ed7bf1af3e49203c8b9ee65a6b0fe37a017fd4
marmo002 <marmo_02@hotmail.com>
2018-04-05 11:22:19 -0400

    created method to display all list inside groceries with asterisks in front the word, added an item, adn printed the list again, check if list has item and display message accordingly, display second item on array and print list again, delete item from list, and print the list

--- exercise9.rb
+++ exercise9.rb
@@ -1,1 +1,55 @@
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+
+def print_list(array)
+  array.each { |e| puts "* #{e}" }
+end
+
+print_list(grocery_list)
+puts "****************************************\n\n"
+
+grocery_list.push("rice")
+
+print_list(grocery_list)
+puts "****************************************\n\n"
+
+puts "Total groceries = #{grocery_list.length}"
+
+def do_i_have?(array, item)
+  message = 0
+  array.each do |e|
+    if e == item
+      message += 1
+    end
+  end
+  if message > 0
+    "Don't need #{item} today"
+  else
+    "You need to pick up #{item}"
+  end
+end
+
+def do_i_have2?(array, item)
+
+  array.include?(item) ? "Don't need #{item} today" : "You need to pick up #{item}"
+
+end
+
+puts do_i_have?(grocery_list, "bananas")
+puts do_i_have2?(grocery_list, "bananas")
+puts "****************************************\n\n"
+
+puts grocery_list[1], "\n\n"
+
+def print_list(array)
+  array.sort.each { |e| puts "* #{e}" }
+end
+
+print_list(grocery_list)
+puts "****************************************\n\n"
+
+def delete_from(item, array)
+  array.delete(item)
+  array.each { |e| puts "* #{e}" }
+end
+
+delete_from("salmon", grocery_list)
