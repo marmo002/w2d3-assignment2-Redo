2cc3815a393f54b9900953ca0ce65c31745e83c5
marmo002 <marmo_02@hotmail.com>
2018-04-05 19:38:13 -0400

    created a method that will get the dogs that are out on bound and call them out

--- exercise14Bonus.rb
+++ exercise14Bonus.rb
@@ -6,6 +6,22 @@ my_dogs = [
   { :name => 'Tobby', :position => 15 }
 ]
 
+neighbor_dogs1 = [
+  { name: 'Fido', position: 9 },
+  { name: 'Rozz', position: 18 },
+  { name: 'Shrek', position: 20 },
+  { name: 'Fiona', position: 2 },
+  { name: 'Donkey', position: 100 }
+]
+
+neighbor_dogs2 = [
+  { name: 'Peter', position: 3 },
+  { name: 'Ray', position: 10 },
+  { name: 'Egon', position:4 },
+  { name: 'Louis', position: 21 },
+  { name: 'Dana', position: 9 }
+]
+puts
 def get_absent_dogs(array)
   dogs_out = []
   array.each do |hash|
@@ -17,3 +33,21 @@ def get_absent_dogs(array)
 end
 
 puts get_absent_dogs(my_dogs)
+puts
+
+def call_absent_dogs(array)
+  absent_dogs = get_absent_dogs(array)
+
+  absent_dogs.each do |absent|
+    puts "Come back, #{absent}!"
+  end
+end
+
+call_absent_dogs(my_dogs)
+puts
+
+call_absent_dogs(neighbor_dogs1)
+puts
+
+call_absent_dogs(neighbor_dogs2)
+puts
